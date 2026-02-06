904e3d05bc16c386be367606bf150492f333b868
yuligryb <118086373+yuligryb@users.noreply.github.com>
2026-02-06 02:08:04 -0500

    Update game.js

--- game.js
+++ game.js
@@ -24,3 +24,30 @@ const config = {
     }
 };
 
+// ========================================
+// GAME VARIABLES
+// ========================================
+let player;
+let stars;
+let asteroids;
+let score = 0;
+let scoreText;
+let gameOver = false;
+let gameWon = false;
+let cursors;
+let wasd;
+let gameWidth;
+let gameHeight;
+
+
+// ========================================
+// PRELOAD - Load Images
+// ========================================
+function preload() {
+    // Load images from Phaser Labs CDN
+    this.load.image('sky', 'https://labs.phaser.io/assets/skies/space3.png');
+    this.load.image('ship', 'https://labs.phaser.io/assets/sprites/thrust_ship2.png');
+    this.load.image('star', 'https://labs.phaser.io/assets/sprites/star.png');
+    this.load.image('asteroid', 'https://labs.phaser.io/assets/sprites/purple_ball.png');
+}
+
